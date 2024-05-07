bb72804f4803e4c0bacc412ea9e7d682d1672d70
geongun20 <leesg9@snu.ac.kr>
2024-05-07 23:29:02 +0900

    add --snu-endpoint option in getData.sh script

--- scripts/getData.sh
+++ scripts/getData.sh
@@ -10,6 +10,23 @@
 datasetName=${1-sampleData}
 numVGAViews=${2-1} #Specify the number of vga views you want to donwload. Up to 480
 numHDViews=${3-31} #Specify the number of hd views you want to donwload. Up to 31
+endpoint="http://domedb.perception.cs.cmu.edu"
+
+
+while [[ -n "$1" ]]; do
+  case "$1" in
+    --snu-endpoint)
+      endpoint="http://vcl.snu.ac.kr/panoptic"
+      ;;
+    -*)
+      echo "Error: Unknown option $1" >&2
+      print_usage
+      exit 1
+      ;;
+    *)
+  esac
+  shift
+done
 
 # Select wget or curl, with appropriate options
 if command -v wget >/dev/null 2>&1; then 
@@ -44,7 +61,7 @@ do
 	fileName=$(printf "vga_%02d_%02d.mp4" ${panels[c]} ${nodes[c]})
 	
   	echo $fileName;
-	cmd=$(printf "$WGET $mO vgaVideos/$fileName http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/videos/vga_shared_crf10/$fileName || rm -v vgaVideos/$fileName")
+	cmd=$(printf "$WGET $mO vgaVideos/$fileName $endpoint/webdata/dataset/$datasetName/videos/vga_shared_crf10/$fileName || rm -v vgaVideos/$fileName")
 	eval $cmd
 done
 
@@ -57,43 +74,43 @@ panel=0
 nodes=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
 for (( c=0; c<$numHDViews; c++))
 do
-  fileName=$(printf "hdVideos/hd_%02d_%02d.mp4" ${panel} ${nodes[c]})
+  fileName=$(printf "hd_%02d_%02d.mp4" ${panel} ${nodes[c]})
   echo $fileName;
   #Download and delete if the file is blank
-	cmd=$(printf "$WGET $mO hdVideos/hd_%02d_%02d.mp4 http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/videos/hd_shared_crf20/hd_%02d_%02d.mp4 || rm -v $fileName" ${panel} ${nodes[c]} ${panel} ${nodes[c]})
+	cmd=$(printf "$WGET $mO hdVideos/$fileName $endpoint/webdata/dataset/$datasetName/videos/hd_shared_crf20/$fileName || rm -v hdVideos/$fileName")
 	eval $cmd
 done
 
 
 # Download calibration data
-$WGET $mO calibration_${datasetName}.json http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/calibration_${datasetName}.json || rm -v calibration_${datasetName}.json
+$WGET $mO calibration_${datasetName}.json $endpoint/webdata/dataset/$datasetName/calibration_${datasetName}.json || rm -v calibration_${datasetName}.json
 
 # MPI version is obsolte and no longer supported
 # Download 3D pose reconstruction results (MPI version, by hd index) 
-#$WGET http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/hdPose3d_stage1.tar
+#$WGET $endpoint/webdata/dataset/$datasetName/hdPose3d_stage1.tar
 # Download 3D pose reconstruction results (MPI version, by vga index)
-#$WGET http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/vgaPose3d_stage1.tar
+#$WGET $endpoint/webdata/dataset/$datasetName/vgaPose3d_stage1.tar
 
 # 3D Body Keypoint (Coco19 keypoint definition)
 # Download 3D pose reconstruction results (by vga index, coco19 format)
 if [ ! -f hdPose3d_stage1_coco19.tar ]; then
-$WGET $mO hdPose3d_stage1_coco19.tar  http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/hdPose3d_stage1_coco19.tar || rm -v hdPose3d_stage1_coco19.tar 
+$WGET $mO hdPose3d_stage1_coco19.tar  $endpoint/webdata/dataset/$datasetName/hdPose3d_stage1_coco19.tar || rm -v hdPose3d_stage1_coco19.tar 
 fi
 
 # 3D Face 
 if [ ! -f hdFace3d.tar ]; then
-$WGET $mO hdFace3d.tar http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/hdFace3d.tar || rm -v hdFace3d.tar 
+$WGET $mO hdFace3d.tar $endpoint/webdata/dataset/$datasetName/hdFace3d.tar || rm -v hdFace3d.tar 
 fi
 
 # 3D Hand
 if [ ! -f hdHand3d.tar ]; then
-$WGET $mO hdHand3d.tar http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/hdHand3d.tar || rm -v hdHand3d.tar
+$WGET $mO hdHand3d.tar $endpoint/webdata/dataset/$datasetName/hdHand3d.tar || rm -v hdHand3d.tar
 fi
 
 
 # 3D Face Fitting Output
 if [ ! -f hdMeshTrack_face.tar ]; then
-$WGET $mO hdMeshTrack_face.tar http://domedb.perception.cs.cmu.edu/webdata/dataset/$datasetName/hdMeshTrack_face.tar || rm -v hdMeshTrack_face.tar 
+$WGET $mO hdMeshTrack_face.tar $endpoint/webdata/dataset/$datasetName/hdMeshTrack_face.tar || rm -v hdMeshTrack_face.tar 
 fi
 
 
